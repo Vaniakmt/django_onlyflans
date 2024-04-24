b4efc9fd21206fe7fbb70f0b0284e643774f0213
Vania <v.montenegro.toroco02@gmail.com>
2024-04-24 19:35:34 -0400

    Tercer hito

--- onlyflans_dj/onlyflans_dj/urls.py
+++ onlyflans_dj/onlyflans_dj/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import indice, acerca , bienvenido
+from web.views import indice, acerca , bienvenido , contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", indice, name="indice"),
     path("acerca", acerca, name="acerca"),
     path("bienvenido", bienvenido, name="bienvenido"),
+    path("contact", contact, name="contact"),
+    
+
     
 ]
--- onlyflans_dj/web/views.py
+++ onlyflans_dj/web/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import ContactForm, Flan
+from .forms import ContactFormForm
+
 
 # Create your views here.
 def indice(request):
-    return render(request, 'index.html', {})
+    flanes = Flan.objects.all()
+    flanes_pub = Flan.objects.filter(is_private=False)
+    return render(request, 'index.html', {"flanes_pub": flanes_pub})
 
 def acerca(request):
     return render(request, 'about.html', {})
 
 def bienvenido(request):
-    return render(request, 'welcome.html', {})
+    flanes_priv = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html', {"flanes_pub": flanes_priv})
+
+def contact(request):
+    if request.method == "POST":
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+
+            return HttpResponseRedirect("/")
+    else:
+        form = ContactFormForm
+    return render(request, "contact.html",{"form": form})
